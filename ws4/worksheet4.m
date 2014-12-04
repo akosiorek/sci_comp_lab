365c4f54df9889539cb536e94c464eceeb6fb6e9
Alberto PérezdeAlba Ortíz <apdealbao@gmail.com>
2014-12-04 10:51:40 +0100

    +subplot labels +stable cases -PlotResult
    Faster

--- ws4/worksheet4.m
+++ ws4/worksheet4.m
@@ -3,12 +3,11 @@ function [] = worksheet4()
     close all
     clear
     clc
-
-    Lx = 1; Ly = 1; % Set dimensions of domain
+    Lx=1; Ly=Lx;
     Nx = [3, 7, 15, 31]; Ny = Nx;   % Arrays of Nx, Ny values
     N = {'3', '7', '15', '31'}; % N: string version
     deltas = 2.^(-4: -1: -10);  % Array of timesteps
-    %dels = {'1/64', '1/128', '1/256', '1/512', '1/1024', '1/2048', '1/4096'};   % deltas: string version
+    dels = {'1/64', '1/128', '1/256', '1/512', '1/1024', '1/2048', '1/4096'};   % deltas: string version
     
     dispTimes = [1/8, 2/8, 3/8, 4/8];   % Create array of display times
     dT = {'1/8', '2/8', '3/8', '4/8'};   % Display times: string version
@@ -21,20 +20,47 @@ function [] = worksheet4()
         set(gcf, 'color', 'white');
     end
     
+    %Create matrix to mark stable cases
+    stability=ones(length(Nx),length(deltas));
+    
+    fprintf('Solving Explicit Euler scheme ... \n\n');
     for i = 1: length(Nx)
-        disp(strcat('Solving for Nx = ', N(i)));
+        fprintf(strcat('Solving for Nx=Ny=', N{i}, ' ... \n\n' ));
         for j = 1: length(deltas)
-            T = ones(Ny(i)+2, Nx(i)+2); % Create T-matrix
-            T(1:Ny(i)+2, 1) = 0; T(1:Ny(i)+2, Nx(i)+2) = 0; T(1, 1:Nx(i)+2) = 0; T(Ny(i)+2, 1:Nx(i)+2) = 0; % Initializa T-matrix
-            time = 0; k = 1;  % Initialize time to zero, set counter for display control
-            while time < dispTimes(length(dispTimes))
-                T = ExpEulTemporalSolver(deltas(j), time, dispTimes(k), T, Lx, Ly);  % Call Explicit Euler Temporal Solver and update T
-                figure(figH(k));    % Activate figure for appropriate dispTime
-                set(figH(k), 'Visible', 'off'); % Hide them temporarily
-                index = j + (i-1)*length(deltas); % Set appropriate index in subplot
-                PlotResults(T, Nx(i), Ny(i), index, length(Nx), length(deltas));
-                time = dispTimes(k);
-                k = k + 1;
+            T = [zeros(1,Nx(i)+2); zeros(Nx(i),1),ones(Nx(i),Nx(i)), zeros(Nx(i),1); zeros(1,Nx(i)+2)]; %Initialize T matrix 
+            for k=1:length(dispTimes)
+                T = ExpEulTemporalSolver(deltas(j), dispTimes(k)-1/8, dispTimes(k), T, Lx, Ly);  % Call Explicit Euler Temporal Solver and update T
+                x = 0: 1/(Nx(i)+1): 1;   % Set x and y
+                y = 0: 1/(Ny(i)+1): 1;
+                
+                figure(figH(k)); %Set new figure
+                rows=length(Nx); %Set subplot rows and columns
+                columns=length(deltas);
+                handles=zeros(rows,columns);
+                handles(i,j) = subplot(rows,columns,j+(i-1)*columns);
+                mesh(x, y, T); %Surface plot
+                pbaspect([1 1 1]);
+                xlabel('x');
+                ylabel('y');
+                zlabel('T');           
+                
+                %Subplot column labels
+                if i == 1
+                    text(0.25, 1.25, strcat('dt=', dels{j}), 'Units', 'normalized', 'FontSize', 12);
+                end
+                %Subplot row labels
+                if j == 1
+                    text(-1.1, 0.5, strcat('Nx=Ny=', N{i}), 'Units', 'normalized', 'FontSize', 12);
+                end
+                
+                set(figH(k), 'Visible', 'off'); % Hide figure temporarily
+                
+                %Indicate if case is stable 
+                %*considering that unstability causes oscillations that make T < 0
+                if (any(T(:) < 0))
+                stability(i,j)=0;
+                end
+              
             end
         end
     end
@@ -43,4 +69,11 @@ function [] = worksheet4()
         figure(figH(i));    % Activate figure i
         suptitle(strcat('State of Solutions at t = ', dT(i)));  % Set title
     end
+    
+    %Display stability matrix
+    dtlabels='dt=1/64 dt=1/128 dt=1/256 dt=1/512 dt=1/1024 dt=1/2048 dt=1/4096';
+    Nlabels='Nx=Ny=3 Nx=Ny=7 Nx=Ny=15 Nx=Ny=31';
+    
+    printmat(stability,'Stable Cases for Explicit Euler Scheme', Nlabels, dtlabels)
+    
 end
